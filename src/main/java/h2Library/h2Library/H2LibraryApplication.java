552196527e214aa61521e1054791c2e4c3cb8d12
TaeHeyeon_Kim <thk98k@gmail.com>
2022-11-02 22:22:17 +0900

    start project

---
+++ src/main/java/h2Library/h2Library/H2LibraryApplication.java
@@ -0,0 +1,13 @@
+package h2Library.h2Library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2LibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2LibraryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/h2Library/h2Library/H2LibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package h2Library.h2Library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class H2LibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
